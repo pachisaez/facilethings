7e412eb6b8f7a53744f00fb71b0ea111b1651e8c
Francisco Sáez <francisco.saez@facilethings.com>
2015-01-02 13:23:59 +0100

    Split traffic data

--- lib/facilethings/partners/coupons_data.rb
+++ lib/facilethings/partners/coupons_data.rb
@@ -2,14 +2,7 @@ require 'facilethings/base'
 
 module Facilethings	
 	class CouponsData < Facilethings::Base
-		attr_reader :user, :status, :coupon
-    attr_accessor :currency, :income, :commission, :cleared, :date
-
-    #def convert_to(currency, rate)
-    #	@attrs[:currency] = currency
-    #	@attrs[:income] = (@attrs[:income]*rate).round(2)
-    #	@attrs[:commission] = (@attrs[:commission]*rate).round(2)
-    #	@attrs[:cleared] = (@attrs[:cleared]*rate).round(2)
-    #end
+    attr_reader :user, :status, :coupon, :date
+    attr_accessor :currency, :income, :commission, :cleared
 	end
 end
--- lib/facilethings/partners/traffic_data.rb
+++ lib/facilethings/partners/traffic_data.rb
@@ -2,6 +2,7 @@ require 'facilethings/base'
 
 module Facilethings	
 	class TrafficData < Facilethings::Base
-    attr_accessor :status, :currency, :income, :commission, :cleared
+    attr_reader :user, :status, :date
+    attr_accessor :currency, :income, :commission, :cleared
 	end
 end
