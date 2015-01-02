aa51e32b546661c46ad91f5b1223e26b08b09e02
Francisco Sáez <francisco.saez@facilethings.com>
2015-01-02 18:24:26 +0100

    Traffic data splitted

--- lib/facilethings/partners/traffic_data.rb
+++ lib/facilethings/partners/traffic_data.rb
@@ -2,7 +2,7 @@ require 'facilethings/base'
 
 module Facilethings	
 	class TrafficData < Facilethings::Base
-    attr_reader :user, :status, :date
+    attr_reader :user, :status, :date 
     attr_accessor :currency, :income, :commission, :cleared
 	end
 end
