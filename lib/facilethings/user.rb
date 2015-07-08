a38d01397358152497971ae924cc4b1249e8b402
Francisco Sáez <francisco.saez@facilethings.com>
2015-07-08 10:20:29 +0200

    User avatar filename

--- lib/facilethings/user.rb
+++ lib/facilethings/user.rb
@@ -3,6 +3,16 @@ require 'facilethings/base'
 module Facilethings	
 	class User < Facilethings::Base
     attr_reader :language, :time_zone, :info, :mail, :role, :created_at
-    attr_reader :first_name, :last_name
+    attr_reader :first_name, :last_name, :avatar
+
+    def avatar_filename
+    	if avatar and (avatar[:id] and avatar[:filename])
+	    	parts = avatar[:filename].split('.')
+	    	filename = parts[0]+"_thumb."+parts[1]
+	    	"https://s3.amazonaws.com/ft-dev/avatars/#{avatar[:id]}/#{filename}"
+	    else
+	    	"https://s3.amazonaws.com/FacileThings/default/thumbnail_thumb.png"
+	    end
+    end
 	end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -9,7 +9,8 @@ describe Facilethings::User do
 		it "should build get methods for all attributes" do
 			user = Facilethings::User.new(@client, { :id => 1, :language => "es", 
 				:time_zone => "Madrid", :info => "Pachi", :mail => "pachisaez@hotmail.com",
-				:first_name => "Francisco", :last_name => "Saez" })
+				:first_name => "Francisco", :last_name => "Saez", 
+				:avatar => { :id => 355, :filename => "avatar.jpg" } })
 
 			expect(user.id).to eq(1)
 			expect(user.language).to eq("es")
@@ -18,6 +19,7 @@ describe Facilethings::User do
 			expect(user.mail).to eq("pachisaez@hotmail.com")
 			expect(user.first_name).to eq("Francisco")
 			expect(user.last_name).to eq("Saez")
+			expect(user.avatar).to eq({ :id => 355, :filename => "avatar.jpg" })
 		end
 		it "should not build set methods for reader attributes" do
 			user = Facilethings::User.new(@client)
@@ -29,6 +31,20 @@ describe Facilethings::User do
 			expect{user.mail = "pachisaez@yahoo.es"}.to raise_error
 			expect{user.first_name = "Fran"}.to raise_error
 			expect{user.last_name = "Saez"}.to raise_error
+			expect{user.avatar = { :id => "2" } }.to raise_error
 		end
 	end
+
+	describe "#avatar_filename"
+		it "should return the URL for the image if exists" do
+			user = Facilethings::User.new(@client, { :id => 1, 
+				:avatar => { :id => 355, :filename => "avatar.jpg" } })
+
+			expect(user.avatar_filename).to eq("https://s3.amazonaws.com/ft-dev/avatars/355/avatar_thumb.jpg")
+		end
+		it "should return the default URL if there's no avatar" do
+			user = Facilethings::User.new(@client)
+
+			expect(user.avatar_filename).to eq("https://s3.amazonaws.com/FacileThings/default/thumbnail_thumb.png")
+		end
 end
