47ccf48f2fe38982a7ceddfd1a56a3799320642a
Francisco Sáez <francisco.saez@facilethings.com>
2015-07-28 22:47:49 +0200

    Added find_user parameter for apps testing purposes

--- lib/facilethings/rest/user.rb
+++ lib/facilethings/rest/user.rb
@@ -3,7 +3,8 @@ require 'facilethings/user'
 module Facilethings
   module REST
     module User
-      def find_user
+      def find_user(username=nil)
+      	# username is only used for testing purposes, so we can set different data in the external adapter
         get_with_object("/v1/users.json", {}, Facilethings::User, :user)
       end
     end
