c1ce04d5f0f2eb50f84e80c43697c253d2e1d77a
Francisco Sáez <francisco.saez@facilethings.com>
2016-04-24 20:23:11 +0200

    API users/:id updated

--- spec/rest/user_spec.rb
+++ spec/rest/user_spec.rb
@@ -7,11 +7,11 @@ describe Facilethings::REST::User do
 
   describe ".find_user" do
   	before do
-    	stub_get('/v1/users.json').to_return(:body => fixture('user.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
+    	stub_get('/v1/users/1.json').to_return(:body => fixture('user.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
   	end
     it 'requests the correct resource' do
-      user = @client.find_user
-      expect(a_get('/v1/users.json')).to have_been_made
+      user = @client.find_user(1)
+      expect(a_get('/v1/users/1.json')).to have_been_made
       expect(user).to be_a Facilethings::User
       expect(user.id).to eq 1
 	  end
