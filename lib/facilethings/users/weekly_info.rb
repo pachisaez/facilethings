91aaa9c9e969941c4a8e8837b6034220a0441da2
Francisco Sáez <francisco.saez@facilethings.com>
2016-10-23 16:18:37 +0200

    Users::WeeklyInfo ok

--- lib/facilethings/users/weekly_info.rb
+++ lib/facilethings/users/weekly_info.rb
@@ -2,7 +2,7 @@ require 'facilethings/base'
 
 module Facilethings	
 	class WeeklyInfo < Facilethings::Base
-    attr_reader :goal_worked_the_most, :area_worked_the_most
+    attr_reader :goal_worked_the_most, :area_worked_the_most, :ok
     attr_integer :important_stuff, :completed_projects, :collected_stuff, :prev_collected_stuff, :done_stuff, :prev_done_stuff
     attr_float :processed_stuff, :prev_processed_stuff, :time_stuff, :prev_time_stuff 
     attr_objects :goals
