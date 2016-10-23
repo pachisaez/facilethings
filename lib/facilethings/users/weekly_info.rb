8d11250611328caffbc66ae22b92214245abb53a
Francisco Sáez <francisco.saez@facilethings.com>
2016-10-23 17:56:24 +0200

    Users::WeeklyInfo every

--- lib/facilethings/users/weekly_info.rb
+++ lib/facilethings/users/weekly_info.rb
@@ -4,6 +4,7 @@ module Facilethings
 	class WeeklyInfo < Facilethings::Base
     attr_reader :goal_worked_the_most, :area_worked_the_most, :ok
     attr_integer :important_stuff, :completed_projects, :collected_stuff, :prev_collected_stuff, :done_stuff, :prev_done_stuff
+    attr_integer :collected_every, :processed_every, :done_every
     attr_float :processed_stuff, :prev_processed_stuff, :time_stuff, :prev_time_stuff 
     attr_objects :goals
 	end
