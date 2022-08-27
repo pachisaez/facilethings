e40771e03c981e574064579681772d2ee138f357
Francisco Sáez <francisco.saez@facilethings.com>
2022-08-27 12:20:20 +0200

    ft1 - find_coupon_by_code

--- lib/facilethings/rest/coupon.rb
+++ lib/facilethings/rest/coupon.rb
@@ -7,6 +7,10 @@ module Facilethings
 	      get_with_object("/v1/coupons/#{id}.json", {}, Facilethings::Coupon, :coupon)
     	end
 
+      def find_coupon_by_code(code)
+        get_with_object("/v1/coupons/#{code}.json", {}, Facilethings::Coupon, :coupon)
+      end
+
       def find_coupons
         get_with_objects("/v1/coupons.json", Facilethings::Coupon, :coupon)
       end
